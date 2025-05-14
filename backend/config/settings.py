8b4854c9342a03a7fe0503d8a52f74313d63b470
C4tLine <notvanger@mail.ru>
2025-05-14 17:48:38 +0300

    Initial. Reload project

---
+++ backend/config/settings.py
@@ -0,0 +1,17 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    KC_BACKEND_SECRET: SecretStr
+
+    LOGS_FILE: str = 'logs.log'
+
+    model_config = SettingsConfigDict(
+        env_file='config/.env',
+        extra='ignore',
+    )
+
+
+settings = Settings()
